e03b756b1260397b015a00f751444ccb23edb7ae
Ahmet Inan <ainan@mathematik.uni-freiburg.de>
2013-11-04 16:00:09 +0100

    added install.sh and updated README
    tested and working on linux-3.9.9

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+KERNEL=$(uname -r)
+#KERNEL=3.9.9-ainan
+ARCH=k1om
+
+make KERNEL_VERSION=$KERNEL MIC_CARD_ARCH=$ARCH
+make KERNEL_VERSION=$KERNEL MIC_CARD_ARCH=$ARCH sysconfdir=/etc includedir=/usr/include install
+
